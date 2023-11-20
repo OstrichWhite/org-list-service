3ba6f4cc61c127bee1a888e0aa202ef3689f5fec
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-20 23:59:21 +0530

    Introduced catchAsyncErr.

--- backend/helpers.js
+++ backend/helpers.js
@@ -1,8 +1,11 @@
-
 exports.runSteps = async (steps, context = {}) => {
   const result = [];
   for (let i = 0; i < steps.length; i++) {
     result[i] = await steps[i](context);
   }
   return result;
+};
+
+exports.catchAsyncErr = (fn) => (req, res, next) => {
+  fn(req, res, next).catch(next);
 };
