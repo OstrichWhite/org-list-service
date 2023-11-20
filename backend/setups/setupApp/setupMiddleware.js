0e61955f2aeab6673d0014afe1388f9524291a0f
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-20 23:47:50 +0530

    Setup cors.

--- backend/setups/setupApp/setupMiddleware.js
+++ backend/setups/setupApp/setupMiddleware.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const setupMiddleware = ({ app }) => {
+  app.use(cors({ origin: "*" }));
   app.use(express.json({ limit: "10kb" })); //for incoming requests with JSON payloads
   app.use(cookieParser());
 };
