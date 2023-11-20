7c7f5bf1bbf268cbc6c35206c8530bf700fb39ba
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-20 22:51:35 +0530

    Introduced auth router and controllers.

--- backend/setups/setupApp/setupRouter.js
+++ backend/setups/setupApp/setupRouter.js
@@ -1,9 +1,11 @@
 const userRouter = require("../../routes/userRoutes");
 const organizationRouter = require("../../routes/organizationRoutes");
+const authRouter = require("../../routes/authRoutes");
 
 const setupRouter = ({ app }) => {
   app.use("/api/v1/users", userRouter);
   app.use("/api/v1/organizations", organizationRouter);
+  app.use("/api/v1/auth", authRouter);
 
   app.all("*", (req, res) => {
     res.status(404).json({
