b80f5278e8b46dfa6fb120899dfee688f7213b71
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-20 23:43:17 +0530

    Setup Auth middlewares.

--- backend/setups/setupApp/setupRouter.js
+++ backend/setups/setupApp/setupRouter.js
@@ -1,11 +1,14 @@
 const userRouter = require("../../routes/userRoutes");
 const organizationRouter = require("../../routes/organizationRoutes");
 const authRouter = require("../../routes/authRoutes");
+const { protect, restrictTo } = require("../../controllers/authController");
 
 const setupRouter = ({ app }) => {
-  app.use("/api/v1/users", userRouter);
-  app.use("/api/v1/organizations", organizationRouter);
   app.use("/api/v1/auth", authRouter);
+  app.use(protect());
+  app.use("/api/v1/users", userRouter);
+  app.use(restrictTo("admin"));
+  app.use("/api/v1/organizations", organizationRouter);
 
   app.all("*", (req, res) => {
     res.status(404).json({
