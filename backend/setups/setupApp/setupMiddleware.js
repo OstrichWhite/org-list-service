59dda4476aaaef7efb3c00eca10377ef39b59be7
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-20 23:32:35 +0530

    Setup cookie-parser middleware.

--- backend/setups/setupApp/setupMiddleware.js
+++ backend/setups/setupApp/setupMiddleware.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const setupMiddleware = ({ app }) => {
   app.use(express.json({ limit: "10kb" })); //for incoming requests with JSON payloads
+  app.use(cookieParser());
 };
 
 module.exports = setupMiddleware;
