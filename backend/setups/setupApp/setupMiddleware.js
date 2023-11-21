ca25feccf701a4f12b44efc072a42fe5c1371e7e
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-21 20:24:00 +0530

    Refactored cors policy.

--- backend/setups/setupApp/setupMiddleware.js
+++ backend/setups/setupApp/setupMiddleware.js
@@ -2,8 +2,12 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const corsOption = process.env.BASE_URL
+  ? { origin: "http://localhost:3000", credentials: true }
+  : { origin: "*" };
+
 const setupMiddleware = ({ app }) => {
-  app.use(cors({ origin: "*" }));
+  app.use(cors(corsOption));
   app.use(express.json({ limit: "10kb" })); //for incoming requests with JSON payloads
   app.use(cookieParser());
 };
